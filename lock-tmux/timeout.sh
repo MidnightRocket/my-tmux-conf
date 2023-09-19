a78f7f9784fa18a4066f1ff3daf491b4396cfd5b
MidnightRocket <git@midnightrocket.dev>
2023-09-19 10:39:27 +0200

    Improve timings in unlock sequence

--- lock-tmux/timeout.sh
+++ lock-tmux/timeout.sh
@@ -2,17 +2,20 @@
 #
 TMUX_BIN="$(command -v tmux)"
 
-$TMUX_BIN display -d 200 "Press C-l to unlock"
+# $TMUX_BIN display -d 200 "Press C-l to unlock"
+# Wait for C-l sequence
 sleep 0.2;
 if [ "$($TMUX_BIN display -p '#{client_key_table}')" == "try_unlock" ]; then
 
-	$TMUX_BIN switch-client -T unlock_timeout
-	sleep 0.05
+	# $TMUX_BIN switch-client -T unlock_timeout
 
 	if [ "$1" = "--send-prefix" ]; then
-		$TMUX_BIN send-keys C-b
+		$TMUX_BIN send-keys C-b \; switch-client -T unlock_timeout
+	else
+		$TMUX_BIN switch-client -T unlock_timeout
 	fi
 
+	sleep 0.05
 	$TMUX_BIN display-message 'Outer Tmux locked. Quickly press prefix and C-l to unlock'
 #else
 #	$TMUX_BIN display "key table changed"

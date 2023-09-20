63a767cac03f92fbe56af55fda6c772c988a7e22
MidnightRocket <git@midnightrocket.dev>
2023-09-20 21:02:15 +0200

    Further improve timing for lock mode + optimizations
    Reverted script back to using sh, which is more lightweight.
    Also tmux sets a global TMUX_BIN environment variable, which can be used
    by scripts. This is used by lock timeout script, it does not have to
    make the extra call to to `command -v`

--- lock-tmux/timeout.sh
+++ lock-tmux/timeout.sh
@@ -1,8 +1,5 @@
-#!/bin/zsh
-#
-TMUX_BIN="$(command -v tmux)"
+#!/bin/sh
 
-# $TMUX_BIN display -d 200 "Press C-l to unlock"
 # Wait for C-l sequence
 sleep 0.2;
 
@@ -10,8 +7,7 @@ if [ "$1" = "--send-prefix" ]; then
 	SEND_PREFIX_ARG='send-keys C-b;'
 fi
 
-TMUX_ARGS=($TMUX_BIN if-shell -F "#{==:#{client_key_table},try_unlock}" "$SEND_PREFIX_ARG switch-client -T unlock_timeout; run-shell -d 0.01 -bC 'display \"Outer Tmux locked. Quickly press prefix and C-l to unlock\"'" '')
-$TMUX_ARGS
+$TMUX_BIN if-shell -F "#{==:#{client_key_table},try_unlock}" "$SEND_PREFIX_ARG switch-client -T unlock_timeout; run-shell -d 0.07 -bC 'display-message -d 750 \"Outer Tmux locked. Quickly press prefix and C-l to unlock\"'" ''
 exit 0
 
 

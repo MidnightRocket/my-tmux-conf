7503d41230c572c6b2c44b379fe7bc2da1871bbe
MidnightRocket <git@midnightrocket.dev>
2023-10-05 23:35:15 +0200

    Battery change intervals for colors and icon

--- status-line/get-battery.sh
+++ status-line/get-battery.sh
@@ -16,57 +16,58 @@ percentage="$(echo "$raw_battery_info" | grep -o "[0-9]\{1,3\}%" | sed s/%//)"
 
 
 
-
 _get_battery_icon_based_on_charge() {
-	if [ $percentage -ge 95 ]; then
-		printf "%s" "$1"
-	elif [ $percentage -ge 75 ]; then
-		printf "%s" "$2"
-	elif [ $percentage -ge 50 ]; then
-		printf "%s" "$3"
-	elif [ $percentage -ge 25 ]; then
-		printf "%s" "$4"
+	if [ $percentage -ge 90 ]; then
+		printf "%s" ""
+	elif [ $percentage -ge 70 ]; then
+		printf "%s" ""
+	elif [ $percentage -ge 45 ]; then
+		printf "%s" ""
+	elif [ $percentage -ge 20 ]; then
+		printf "%s" ""
 	else
-		printf "%s" "$5"
+		printf "%s" ""
 	fi
 }
 # Using two different function for battery_icon and color, because I 
 # like to have different weighting on them.
 _get_color_based_on_charge() {
-	if [ $percentage -ge 80 ]; then
-		printf "%s" "$1"
-	elif [ $percentage -ge 65 ]; then
-		printf "%s" "$2"
+	if [ $percentage -ge 50 ]; then
+		printf "%s" "color46"
 	elif [ $percentage -ge 40 ]; then
-		printf "%s" "$3"
-	elif [ $percentage -ge 15 ]; then
-		printf "%s" "$4"
+		printf "%s" "color154"
+	elif [ $percentage -ge 30 ]; then
+		printf "%s" "color226"
+	elif [ $percentage -ge 20 ]; then
+		printf "%s" "color214"
+	elif [ $percentage -ge 10 ]; then
+		printf "%s" "color208"
 	else
-		printf "%s" "$5"
+		printf "%s" "color196"
 	fi
 }
 
 _get_element_based_on_status() {
 	# See https://github.com/tmux-plugins/tmux-battery/blob/5c52d4f7f80384de0781c2277a8989ba98fae507/scripts/battery_icon_status.sh#L40
 	if [[ $status =~ (charged) || $status =~ (full) ]]; then
-		printf "%s" "$1"
+		printf "%s" "󰚥"
 	elif [[ $status =~ (^charging) ]]; then
-		printf "%s" "$2"
+		printf "%s" "󱐋"
 	elif [[ $status =~ (^discharging) ]]; then
-		printf "%s" "$3"
+		printf "%s" ""
 	elif [[ $status =~ (attached) ]]; then
-		printf "%s" "$4"
+		printf "%s" "󱐥"
 	else
-		printf "%s" "$5"
+		printf "%s" "⁉︎"
 	fi
 }
 
-battery_icon="$(_get_battery_icon_based_on_charge "" "" "" "" "")"
+battery_icon="$(_get_battery_icon_based_on_charge)"
 
-percent_color="$(_get_color_based_on_charge "color46" "color226" "color214" "color202" "color160")"
+percent_color="$(_get_color_based_on_charge)"
 
-status_icon="$(_get_element_based_on_status "󰚥" "󱐋" "" "󱐥" "⁉︎")"
+status_icon="$(_get_element_based_on_status)"
 
 
 
-printf "#[push-default]#{?client_prefix,,#[fg=%s]}%s%s %s%%#{?client_prefix,,#[fg=default]}#[pop-default]" "$percent_color" "$status_icon" "$battery_icon" "$percentage"
+printf "#[push-default]#{?client_prefix,,#[fg=%s]}%s%s %s%%#{?client_prefix,,#[fg=default,default]}#[pop-default]" "$percent_color" "$status_icon" "$battery_icon" "$percentage"

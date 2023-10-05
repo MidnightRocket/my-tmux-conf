56ada17570a07e5530f92504b9446277c5d03bc6
MidnightRocket <git@midnightrocket.dev>
2023-10-05 14:34:57 +0200

    Improve testing script
    Now the script automatically goes into a nested testing session, if it
    detects that it is run within a tmux session.

--- testing.sh
+++ testing.sh
@@ -4,5 +4,15 @@
 # unset TMUX; SSH_CLIENT="simulated" tmux -f ./tmux.conf -L testing-nested new -s nested; tmux -L testing-nested kill-session
 
 
+if [ -n "$TMUX" ]; then
+	name="testing-nested"
+	unset TMUX
+	export SSH_CLIENT="simulated"
+else
+	name="testing"
+fi
 
-tmux -f ./tmux.conf -L testing new -s testing; tmux -L testing kill-session
+
+
+tmux -f ./tmux.conf -L "$name" new -s "$name"
+tmux -L "$name" kill-session

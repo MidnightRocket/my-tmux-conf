a6e727c62af3ac16542414962ee77af20aa503ff
MidnightRocket <git@midnightrocket.dev>
2023-10-26 18:16:59 +0200

    Make testing script use relative path to script location
    This means that the testing script no longer requires working directory
    to be in tmux conf folder.

--- testing.sh
+++ testing.sh
@@ -3,6 +3,7 @@
 # To start a simulated ssh tmux session use:
 # unset TMUX; SSH_CLIENT="simulated" tmux -f ./tmux.conf -L testing-nested new -s nested; tmux -L testing-nested kill-session
 
+readonly BASEDIR=$(realpath "$(dirname "$0")")
 
 if [ -n "$TMUX" ]; then
 	name="testing-nested"
@@ -14,5 +15,5 @@ fi
 
 
 
-tmux -f ./tmux.conf -L "$name" new -s "$name"
+tmux -f "$BASEDIR/tmux.conf" -L "$name" new -s "$name"
 tmux -L "$name" kill-session

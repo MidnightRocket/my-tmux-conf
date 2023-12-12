e0c699068d7a9757bf692c4d146a39fcaeb84988
MidnightRocket <git@midnightrocket.dev>
2023-12-12 14:45:18 +0100

    Add dynamic snippets menu
    This new dynamic snippets menu, can generate a snippets menu from a
    .snippets file.
    This makes it much easier to dynamically add new snippets.

---
+++ snippets-menu/helper.awk
@@ -0,0 +1,35 @@
+#!/usr/bin/awk -f
+# https://stackoverflow.com/a/1418292
+BEGIN {
+	ORS=" "
+}
+
+function getQuotedField(fieldNum) {
+	if ($fieldNum ~ /^[^"]/) return $fieldNum # if first field does no begin with ", then return just that field
+	output = ""
+	for (i = fieldNum; i <= NF; i++) {
+		lastField = i
+		output = output OFS $i
+		if ($i ~ /[^\\]"$/) {
+			break
+		}
+	}
+	return substr(output, 2)
+}
+
+function getFieldRange(start, end) {
+	output = ""
+	for (i = start; i <= end; i++) {
+		output = output OFS $i
+	}
+	return substr(output, 2) # remove extra space at the beginning
+}
+
+/^''/ {
+	print "''"
+	next # skip to next line: https://stackoverflow.com/a/8723046
+}
+
+/^[^#]/ {
+	print getQuotedField(1), $(++lastField), "{send-keys " getFieldRange(++lastField, NF) "}"
+}
--- snippets-menu/open-menu.sh
+++ snippets-menu/open-menu.sh
@@ -1,21 +1,31 @@
-#!/usr/bin/env zsh
-#
+#!/bin/sh
+set -eu
 
-BASEDIR=$(realpath "$(dirname "$0")")
-line="$(< "${BASEDIR}/main.snippets")"
-line_arr=(${(f)=line})
+BASEDIR="$(realpath "$(dirname -- "$0")")"
+SNIPPETS_FILE="${BASEDIR}/main.snippets"
+CACHE_DIR="${BASEDIR}/.cache"
+CACHE_FILE="${CACHE_DIR}/snippets.tmux.conf"
 
-printargs() {
-	printf "- %s\n" $@
-}
 
-display_menu='display-menu -T "#[align=centre] Snippets Menu " -x "#{e|-:#{client_width},#{e|+:#{popup_width},5}}" -y "#{e|+:#{popup_height},2}" -- '
+tmux="${TMUX_BIN:-"$(command -v tmux)"}"
 
-display_args=""
 
-for l in $line_arr; do
-	display_args+=$(printf "%s" "$l" | xargs -0 "${BASEDIR}/helper.sh")
-done
+if [ ! -e "$CACHE_FILE" ] || [ "$CACHE_FILE" -ot "$SNIPPETS_FILE" ]; then
+	AWK_SCRIPT="${BASEDIR}/helper.awk"
 
-tmux_args=$(printf "%s %s" $display_menu $display_args)
-echo $tmux_args | tmux source -
+	if [ ! -d "$CACHE_DIR" ]; then
+		mkdir "$CACHE_DIR"
+	fi
+
+	# tmux display "Generating snippets"
+
+	display_menu='display-menu -T "#[align=centre] Snippets Menu " -x "#{e|-:#{client_width},#{e|+:#{popup_width},5}}" -y "#{e|+:#{popup_height},2}" -- '
+
+	HEADER="\"-         #[align=centre]Quit this menu#[align=right]     (q)\" '' {} ''"
+
+	display_args="$(awk -f "$AWK_SCRIPT" "$SNIPPETS_FILE")"
+
+	printf "%s %s %s" "$display_menu" "$HEADER" "$display_args" > "$CACHE_FILE"
+fi
+
+exec $tmux source "$CACHE_FILE"

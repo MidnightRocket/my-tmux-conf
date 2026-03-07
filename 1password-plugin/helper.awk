9774d0ca898df8fa5a15d32ff0427e893e2d9e67
MidnightRocket <git@midnightrocket.dev>
2026-03-07 14:33:47 +0100

    Add simple 1password plugin

---
+++ 1password-plugin/helper.awk
@@ -0,0 +1,40 @@
+#!/usr/bin/awk -f
+# Author MidnightRocket https://github.com/MidnightRocket/my-tmux-conf
+#
+# https://stackoverflow.com/a/1418292
+
+
+BEGIN {
+	ORS=" "
+}
+
+function getQuotedField(fieldNum) {
+	if ($fieldNum ~ /^[^"]/) return $fieldNum # if first field does no begin with ", then return just that field
+	output = ""
+	for (i = fieldNum; i <= NF; i++) {
+		lastField = i
+		output = output OFS $i
+		if ($i ~ /[^\\]"$/) {
+			break
+		}
+	}
+	return substr(output, 2)
+}
+
+function getFieldRange(start, end) {
+	output = ""
+	for (i = start; i <= end; i++) {
+		output = output OFS $i
+	}
+	return substr(output, 2) # remove extra space at the beginning
+}
+
+# Detect section seperator
+/^[[:space:]]*(---)/ {
+	print "''"
+	next # skip to next line: https://stackoverflow.com/a/8723046
+}
+
+NF > 0 && !/^[[:space:]]*#/ {
+	print getQuotedField(1), $(++lastField), "{run-shell '" SCRIPT " " getFieldRange(++lastField, NF) "'}"
+}
---
+++ 1password-plugin/open-menu.sh
@@ -0,0 +1,34 @@
+#!/bin/dash
+# Author MidnightRocket https://github.com/MidnightRocket/my-tmux-conf
+
+set -eu
+
+BASEDIR="$(realpath "$(dirname -- "$0")")"
+SNIPPETS_FILE="${BASEDIR}/main.1plist"
+CACHE_DIR="${BASEDIR}/.cache"
+CACHE_FILE="${CACHE_DIR}/1plist.tmux.conf"
+
+
+tmux="${TMUX_BIN:-"$(command -v tmux)"}"
+
+
+if [ ! -e "$CACHE_FILE" ] || [ "$CACHE_FILE" -ot "$SNIPPETS_FILE" ]; then
+	AWK_SCRIPT="${BASEDIR}/helper.awk"
+
+	if [ ! -d "$CACHE_DIR" ]; then
+		mkdir "$CACHE_DIR"
+	fi
+
+	# tmux display "Generating snippets"
+
+	# display_menu='display-menu -T "#[align=centre] 1Password Items " -x "#{e|-:#{client_width},#{e|+:#{popup_width},5}}" -y "#{e|+:#{popup_height},2}" -- '
+	display_menu='display-menu -T "#[align=centre] 1Password Items " -x "#{e|+:#{cursor_x},#{e|/:#{popup_width},2}}" -y "#{e|-:#{cursor_y},#{e|/:#{popup_height},2}}" -- '
+
+	HEADER="\"-         #[align=centre]Quit this menu#[align=right]     (q)\" '' {} ''"
+
+	display_args="$(awk -v SCRIPT="$BASEDIR/insert" -f "$AWK_SCRIPT" "$SNIPPETS_FILE")"
+
+	printf "%s %s %s" "$display_menu" "$HEADER" "$display_args" > "$CACHE_FILE"
+fi
+
+"$tmux" source "$CACHE_FILE"
